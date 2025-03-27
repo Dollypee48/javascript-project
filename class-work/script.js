e7da7652aaedcbd2d0eda62d3975c43a0978f3bb
Dollypee48 <akinolad22@gmail.com>
2025-03-27 12:50:34 +0100

    completion

--- class-work/script.js
+++ class-work/script.js
@@ -1,25 +1,16 @@
-
-percentageIncrease (newAmount - oldAmount / oldAmount) * 100
+// let oldAmount = 2500;
+// let newAmount = 9000;
 
 function calculatePercentageIncrease(oldAmount, newAmount){
-    const percentageIncrease = (oldAmount - newAmount / oldAmount) * 100
-    return percentageIncrease
+    const percentageIncrease = ((newAmount - oldAmount) / oldAmount) * 100
+    return percentageIncrease;
 }
 
-let oldAmount = 2500;
-let newAmount = 9000;
-
-console.log(percentageIncrease)
-
-function calculatePerIncrease(oldPrice, newPrice) {
-
-    let percentageIncrease = ((newPrice - oldPrice) / oldPrice) * 100;
-    
-    console.log(The percentage increase of the data cost is: ${percentageIncrease}%);
-}
-
-  let oldPrice = 2500;
+let oldPrice = 2500;
 let newPrice = 9000;
 
+const result = calculatePercentageIncrease(oldPrice, newPrice);
 
-calculatePerIncrease(oldPrice, newPrice);
+console.log(`The percentage increase in cost is: ${result}%`);
+
+
